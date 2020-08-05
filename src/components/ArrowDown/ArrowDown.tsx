c3fe37677bbed94b2027f2a6181a0dd1ccdc4fff
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-05 15:26:52 -0700

    arrow fix

--- src/components/ArrowDown/ArrowDown.tsx
+++ src/components/ArrowDown/ArrowDown.tsx
@@ -1,7 +1,7 @@
 export default function ArrowDown() {
   return (
     <svg width="16" height="20" xmlns="http://www.w3.org/2000/svg">
-      <g stroke="#000" stroke-width="1.5" fill="none" fill-rule="evenodd">
+      <g stroke="#000" strokeWidth="1.5" fill="none" fillRule="evenodd">
         <path d="M8 0v18M15.071 11L8 18.071.929 11" />
       </g>
     </svg>
