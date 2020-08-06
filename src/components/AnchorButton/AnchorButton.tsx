2c0f19dca3b225309b5a7f0e451909ddf5660730
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-06 14:40:56 -0700

    smaller button

--- src/components/AnchorButton/AnchorButton.tsx
+++ src/components/AnchorButton/AnchorButton.tsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 import { colors, size } from "style";
 
 const AnchorButton = styled.a<{ light?: boolean }>`
-  padding: ${size[0]} ${size[5]};
+  padding: ${size[-1]} ${size[4]};
   border-radius: 999999px;
   background: ${colors.black};
 
