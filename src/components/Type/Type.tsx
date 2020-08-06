ce0a343874cc157b3f7b47b9d927c7cdffe80a0e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-06 13:58:23 -0700

    update type

--- src/components/Type/Type.tsx
+++ src/components/Type/Type.tsx
@@ -13,42 +13,40 @@ const SHARED_PROPS = css<HeadingProps>`
 `;
 
 export const LargeHead = styled.h1`
-${SHARED_PROPS}
+  ${SHARED_PROPS};
   font-size: ${size[4]};
   line-height: 0.8;
 
-  @media ${mq.sm}{
-  font-size: ${size[5]};
-}
+  @media ${mq.sm} {
+    font-size: ${size[5]};
+  }
 `;
 
 export const MediumHead = styled.h3`
-${SHARED_PROPS}
+  ${SHARED_PROPS};
   font-size: ${size[3]};
   line-height: 1.1;
-  max-width:10em;
+  max-width: 10em;
 
-  @media ${mq.sm}{
+  @media ${mq.sm} {
     font-size: ${size[4]};
   }
 `;
 
 export const Support = styled.p`
-  ${SHARED_PROPS}
+  ${SHARED_PROPS};
   color: ${colors.grey};
   line-height: 1.2;
   font-size: ${size[1]};
   max-width: 15em;
 
-  @media ${mq.sm}{
-  font-size: ${size[2]};
+  @media ${mq.sm} {
+    font-size: ${size[2]};
   }
 `;
 
 export const Eyebrow = styled.h2`
-  ${SHARED_PROPS}
+  ${SHARED_PROPS};
   color: ${colors.grey};
-  font-size:${size[0]};
-  margin-bottom: 0;
-
+  font-size: ${size[0]};
 `;
