a8ee37fe081b22cb0c01049106bade6e9065ee63
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-07 17:05:57 -0700

    ???!?!?

---
+++ cms/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Website",
+            apiId: "b60185cf-c229-4244-8393-e4149aad4a36",
+            buildHookId: "5f2c771db2779d0160c5e563",
+            name: "reinvent-www",
+          },
+        ],
+      },
+    },
+  ],
+};
