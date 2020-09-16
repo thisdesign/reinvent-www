4de6086511dfc48a9a0b01ca232e2fcf5ae75498
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-16 11:41:57 -0700

    remove token

--- src/lib/sanity.tsx
+++ src/lib/sanity.tsx
@@ -5,8 +5,6 @@ const options = {
   dataset: "2020-09",
   projectId: "07nl0hko",
   useCdn: false,
-  token:
-    "skRDRLncak5pDNjpTxtAP6qPHYwyI0y0wUVQjPBsyAeQBSbzU4Mhb9Uj3bj2iobJMXMvV694QrWw28P6WpgiR8JAdtArBeHtL5zk216OI1DxCAO6kHgG5uyqjiOJMTG0jP8kqhZzl3NUrMszDOVhEP9mTsdJVAtFGxM3Zr7WZHX4PRGR1iMb",
 };
 
 const client = sanityClient(options);
