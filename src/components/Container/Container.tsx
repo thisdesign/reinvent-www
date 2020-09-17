230dd6ac812accfbc37ef13366a16a549547911f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-17 12:57:12 -0700

    update container

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,8 +1,9 @@
+import { size } from "style";
 import styled from "styled-components";
 
 const Container = styled.div`
   margin: 0 auto;
-  max-width: 1600px;
+  max-width: ${size.maxWidth};
 `;
 
 export default Container;
