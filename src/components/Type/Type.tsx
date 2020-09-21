decf92485e4d7f1a443bd2b110051ce3e7fd89f2
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-21 16:59:46 -0700

    Revert "display to sans serif"
    This reverts commit 19004614c5d6fefa1a9cc16f46337f69923716ea.

--- src/components/Type/Type.tsx
+++ src/components/Type/Type.tsx
@@ -31,6 +31,7 @@ export const IntroHead = styled.h2<HeadingProps>`
 export const HomepageHeroText = styled.h1<HeadingProps>`
   ${SHARED_PROPS};
 
+  font-family: ${fontFamilies.display};
   font-size: ${size[3]};
   line-height: 1;
 
