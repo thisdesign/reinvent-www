7bfd16bb9d7e34a51bfd229545c46e0f4338a063
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-10-14 15:20:56 -0700

    fix about underline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { PageSchema } from "types";
 
 const Home: React.FC<{ data: PageSchema }> = ({ data }) => {
   return (
-    <Layout slug="about" title={null} route="/" image={null} description={null}>
+    <Layout slug="home" title={null} route="/" image={null} description={null}>
       <Modules data={data.modules || []} />
     </Layout>
   );
