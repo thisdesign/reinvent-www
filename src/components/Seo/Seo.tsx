60f28fda881ebe0fe4593c6c0b079f8c9776dbe9
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-10-30 10:33:12 -0700

    update next

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -53,7 +53,26 @@ const Seo: React.FC<SeoProps> = ({
       {description && <meta name="twitter:description" content={description} />}
       {image && <meta name="twitter:image:src" content={image} />}
       <meta name="twitter:url" content={siteUrl} />
-      <Favicon />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
