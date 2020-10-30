5fcb708475ffccde94963a14e224911798d0a63b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-10-30 10:54:20 -0700

    fix intro img

--- src/components/Modules/ModuleIntro/ModuleIntro.Styled.tsx
+++ src/components/Modules/ModuleIntro/ModuleIntro.Styled.tsx
@@ -28,6 +28,7 @@ const Container = styled.div<{ hasImage: boolean; tint: number }>`
         left: 0;
         height: 100%;
         width: 100%;
+        object-fit: cover;
       }
     `}
 `;
--- src/components/Modules/ModuleIntro/ModuleIntro.tsx
+++ src/components/Modules/ModuleIntro/ModuleIntro.tsx
@@ -3,7 +3,6 @@ import { ModuleIntro as ModuleIntroType } from "types";
 import S from "./ModuleIntro.Styled";
 import { PureBlockContent } from "components";
 import { IntroHead } from "components/Type/Type";
-import Image from "next/image";
 import { urlFor } from "lib/sanity";
 
 const ModuleIntro: React.FC<{ data: ModuleIntroType }> = ({ data }) => {
@@ -11,7 +10,7 @@ const ModuleIntro: React.FC<{ data: ModuleIntroType }> = ({ data }) => {
 
   return (
     <S.Container hasImage={!!data.image} tint={data.tint || 0}>
-      {data.image && <Image src={urlFor(data.image).url() || ""} unsized />}
+      {data.image && <img src={urlFor(data.image).width(2400).url() || ""} />}
       <S.Intro>
         <IntroHead center={isCentered}>
           {data.text && (
